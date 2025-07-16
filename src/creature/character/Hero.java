6eb25685d9224e86dfe63ef875bba0dff987b5da
241105 <241105@std.hi-joho.ac.jp>
2025-07-16 10:44:37 +0900

    ExamProject2

--- src/creature/character/Hero.java
+++ src/creature/character/Hero.java
@@ -1,18 +1,12 @@
 package creature.character;
 
+import creature.Creature;
+
 public class Hero extends Character{
     public Hero(String name,int hp,String weapon) {
-        super(name,hp);
-        setWeapon(weapon);
     }
     public void attack(Creature target) {
         System.out.println(getName() + "は" + getWeapon() + "で攻撃！" + target.getName() + "10のダメージを与えた！");
         target.setHp(target.getHp()-10);
-    }
-    public void setWeapon(String weapon){
-        this.weapon = weapon;
-    }
-    public String getWeapon(){
-        return this.weapon;
     }
 }
